e2b0bc77ed101865e6d22d1bc1370e646964ce93
skywang2 <727854256@qq.com>
2022-06-05 12:47:48 +0800

    [add][normal]1.build .a and .so at the same time.

---
+++ cpp_project_linux/project010_cmake_a_so/lib/hello_lib.cpp
@@ -0,0 +1,8 @@
+#include "hello_lib.h"
+#include <iostream>
+
+void HelloFunc()
+{
+  std::cout << "hello world." << std::endl;
+}
+
---
+++ cpp_project_linux/project010_cmake_a_so/lib/hello_lib.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_LIB_H
+#define HELLO_LIB_H
+
+void HelloFunc();
+
+#endif
+
