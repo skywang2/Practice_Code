cef78009bc84cf816438178fa513f2a5e798dc28
skywang2 <727854256@qq.com>
2022-06-05 18:52:57 +0800

    [chg][normal]1.use Multi-Thread with CMake;

--- cpp_project_linux/project009_cmake_async_thread/src/main.cpp
+++ cpp_project_linux/project009_cmake_async_thread/src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <thread>
+#include <unistd.h>
+using std::thread;
+using std::cout;
+using std::endl;
+
+void ThreadFunc(int& a)
+{
+  cout << "thread a:" << ++a << endl;
+}
 
 int main(int argc, char* argv[])
 {
   std::cout << "hello world." << std::endl;
+  int a = 5;
+  thread t(ThreadFunc, std::ref(a));
+  if(t.joinable()) t.join();
+  cout << "main a:" << a << endl;
+
   return 0;
 }
