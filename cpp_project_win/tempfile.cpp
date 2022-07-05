1f93d9b68c75daca94cb35d90ffdd89b4a354dfd
香蕉储蓄所 <727854256@qq.com>
2022-07-05 19:13:36 +0800

    [chg][normal]1.添加简单工厂模式；2.添加经典策略模式；3.将策略模式与简单工厂结合

--- cpp_project_win/tempfile.cpp
+++ cpp_project_win/tempfile.cpp
@@ -1,5 +1,27 @@
 //练习设计模式
-//代码基本来自《大话设计模式》每篇的总结
+//代码基本来自《大话设计模式》每篇的总结，代码使用C++
 
 //简单工厂模式
+class SimpleFactory
+{
+  //使用默认构造函数和析构函数
+  
+  static ParentClass* CreateObj(string type)
+  {
+    ParentClass* parent = nullptr;
+    switch(type.toInt())
+    {
+      case SubClassA:
+        parent = new SubClassA();//向上转型
+        break;
+      case SubClassB:
+        parent = new SubClassB();
+        break;
+      default:
+        break;
+    }
+    return parent;
+  }
+};
 
+//策略模式，定义一组外部调用接口相同的算法
