f96497873f7cc024361180c2f7391546467cfee5
香蕉储蓄所 <727854256@qq.com>
2022-07-05 19:31:22 +0800

    [chg][normal]1.添加经典策略模式；2.策略模式与简单工厂结合

--- cpp_project_win/tempfile.cpp
+++ cpp_project_win/tempfile.cpp
@@ -5,7 +5,7 @@
 class SimpleFactory
 {
   //使用默认构造函数和析构函数
-  
+public:
   static ParentClass* CreateObj(string type)
   {
     ParentClass* parent = nullptr;
@@ -25,3 +25,55 @@ class SimpleFactory
 };
 
 //策略模式，定义一组外部调用接口相同的算法
+class Strategy
+{
+public:
+  bool Algorithm() = 0;//纯虚函数，该类为纯虚类
+};
+
+class StrategyA : public Strategy
+{
+public:
+  bool Algorithm() override { cout << "hello world" << endl;}
+};
+
+//1.经典策略模式，封装一套调用算法的统一接口
+class Context
+{
+public:
+  Context(Strategy s) { this.stg = s; }//由外部构造具体算法对象并传入
+  bool PublicInterface() { return stg.Algorithm(); }
+  Strategy stg;
+};
+
+//2.策略模式与简单工厂结合
+class Context
+{
+public:
+  //由外部指定算法类型，在内部构造算法对象
+  Context(string type)
+    :stg(nullptr)
+  {
+    switch(type.toInt())
+    {
+      case StrategyA:
+        stg = new StrategyA();
+        break;
+      case StrategyB:
+        stg = new StrategyB();
+        break;
+      default:
+        break;
+    }
+  }
+  
+  bool PublicInterface() { return stg->Algorithm(); }
+  Strategy* stg;
+};
+
+
+
+
+
+
+
