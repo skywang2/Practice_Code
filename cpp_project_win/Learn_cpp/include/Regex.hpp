61e16a26b41feea04b88a515d63e3f0073f80c23
skywang2 <727854256@qq.com>
2022-07-05 23:58:23 +0800

    [chg][normal]1.增加regex相关函数使用

--- cpp_project_win/Learn_cpp/include/Regex.hpp
+++ cpp_project_win/Learn_cpp/include/Regex.hpp
@@ -1,5 +1,39 @@
 #ifndef REGEX_HPP
 #define REGEX_HPP
+#include <iostream>
+#include <regex>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+using std::regex;
 
+typedef enum class _STRTYPE
+{
+	HTML = 0,
+
+}STRTYPE;
+
+string GetStrings(STRTYPE type)
+{
+	string s;
+	switch (type)
+	{
+	case STRTYPE::HTML:
+		s = "<p>Hello World</p>";
+	default:
+		break;
+	}
+
+	return std::move(s);
+}
+
+//regex_matchÍêÈ«Æ¥Åä
+void RegexMatch()
+{
+	string s = GetStrings(STRTYPE::HTML);
+	regex reg("<.*>.*</.*>");
+	bool ret = std::regex_match(s, reg);
+}
 
 #endif // !REGEX_HPP
