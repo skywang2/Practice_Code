0585237b5611f58d25f5a8bf1bdfe7eca27e01a7
skywang2 <727854256@qq.com>
2022-09-07 01:24:35 +0800

    [dbg][normal]1.glGetUniformLocation返回-1是因为uniform变量没有对输出产生影响，被优化了，代码未改完

--- cpp_project_win/OpenGLWindow/res/shaders/shader_cube02_fragment.glsl
+++ cpp_project_win/OpenGLWindow/res/shaders/shader_cube02_fragment.glsl
@@ -1,13 +1,15 @@
 #version 330 core
 
 layout(location = 0) out vec4 color;
-uniform sampler2D u_texture;//纹理槽下标
 
 in vec2 v_texCoord;
 in vec4 v_color;
+
+uniform sampler2D u_texture;//纹理槽下标
+
 void main()
 {
 	vec4 texColor = texture(u_texture, v_texCoord);	//槽，纹理坐标
-	color = vec4(v_color.x, v_color.y, v_color.z, 1.0);
+	color = texColor;
 	//color = texColor;
 }
--- cpp_project_win/OpenGLWindow/res/shaders/shader_cube02_vertex.glsl
+++ cpp_project_win/OpenGLWindow/res/shaders/shader_cube02_vertex.glsl
@@ -2,11 +2,13 @@
 
 layout(location = 0) in vec4 position;
 layout(location = 1) in vec2 texCoord;
-uniform mat4 u_MVP;
 
 //输出到片段着色器
 out vec2 v_texCoord;
 out vec4 v_color;
+
+uniform mat4 u_MVP;
+
 void main()
 {
 	gl_Position = u_MVP * vec4(position.x, position.y, position.z, 1.0);
