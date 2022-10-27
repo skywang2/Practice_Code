1fc0b6a3e7cdfb87fea8014b1fb8511e568013f1
skywang2 <727854256@qq.com>
2022-10-28 00:42:03 +0800

    [add][normal]1.增加使用Mesh的测试类

---
+++ cpp_project_win/OpenGLWindow/src/tests/TestMesh1.cpp
@@ -0,0 +1,82 @@
+#include "TestMesh1.h"
+#include "GLFW/glfw3.h"
+
+#include "../LoadModel.h"
+
+extern GLFWwindow* g_window;
+extern MouseParam* g_mouseParam;
+//float lastX = 640, lastY = 360;//鼠标在上一帧位置
+
+namespace tests {
+	TestMesh1::TestMesh1()
+		: display_w(1280)
+		, display_h(720)
+		, model_trans(glm::vec3(0.0f))
+		, fov(45.f)
+		, zNear(1.f), zFar(100.f)
+		, cameraPos(glm::vec3(0.0f, 0.0f, 5.0f))
+		, cameraFront(glm::vec3(0.0f, 0.0f, -0.1f))//使用g_mouseParam.front替代
+		, cameraUp(glm::vec3(0.0f, 1.0f, 0.0f))
+	{
+
+	}
+
+	TestMesh1::~TestMesh1()
+	{
+	}
+
+	void TestMesh1::OnUpdate()
+	{
+		fov = (g_mouseParam) ? g_mouseParam->fov : fov;
+		const glm::vec3& mouseMove = (g_mouseParam) ? g_mouseParam->front : cameraFront;
+
+		proj = glm::perspective(glm::radians(fov), (float)display_w / (float)display_h, zNear, zFar);//透视投影
+		view = glm::lookAt(
+			cameraPos, //相机位置坐标, in World Space
+			//cameraPos + cameraFront, //镜头朝向，用位置+朝向可以使相机移动时朝向固定方向而不是某一点
+			cameraPos + mouseMove,
+			cameraUp//相机上方向（FPS相机上方向默认vec(0.0, 1.0, 0.0)）
+		);
+		model = glm::translate(glm::mat4(1.0f), model_trans);//模型矩阵
+		mvp = proj * view * model;
+		//shader->Bind();
+		//shader->SetUniformMat4f("u_MVP", mvp);//传入MVP矩阵
+		//GL_INVALID_VALUE
+	}
+
+	void TestMesh1::OnRender()
+	{
+		GLCall(glClearColor(0.f, 0.f, 0.f, 1.f));
+		GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+		glClearDepth(99999.f);
+
+	}
+
+	void TestMesh1::OnImGuiRender()
+	{
+	}
+
+	void TestMesh1::ProcessInputClass(GLFWwindow* window)
+	{
+		glm::vec3& mouseMove = (g_mouseParam) ? g_mouseParam->front : cameraFront;
+
+		float cameraSpeed = 0.1f; // adjust accordingly
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		{
+			cameraPos += cameraSpeed * mouseMove;
+		}
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		{
+			cameraPos -= cameraSpeed * mouseMove;
+		}
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		{
+			cameraPos -= glm::cross(mouseMove, cameraUp) * cameraSpeed;
+		}
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		{
+			cameraPos += glm::cross(mouseMove, cameraUp) * cameraSpeed;
+		}
+	}
+
+}
---
+++ cpp_project_win/OpenGLWindow/src/tests/TestMesh1.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "Test.h"
+#include <memory>
+//#include "GL/glew.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "../include/imgui/imgui.h"
+
+#include "../Renderer.h"
+#include "../VertexBuffer.h"
+#include "../IndexBuffer.h"
+#include "../VertexArray.h"
+#include "../VertexBufferLayout.h"
+#include "../Shader.h"
+#include "../Texture.h"
+
+namespace tests {
+
+	//使用assimp读取模型数据
+	//使用自定义Mesh类存储、渲染网格对象
+	class TestMesh1 : public Test
+	{
+	public:
+		TestMesh1();
+		~TestMesh1();
+
+		void OnUpdate() override;
+		void OnRender() override;
+		void OnImGuiRender() override;
+		void ProcessInputClass(GLFWwindow*) override;
+		friend void MouseCallback(GLFWwindow* window, double xpos, double ypos);//鼠标事件处理函数
+		friend void ScrollCallback(GLFWwindow* window);//滚轮事件处理函数
+
+	private:
+		int display_w, display_h;
+
+		glm::vec3 model_trans;//模型变换平移
+		glm::mat4 proj;
+		glm::mat4 view;
+		glm::mat4 model;//模型变换
+		glm::mat4 mvp;
+		float fov, zNear, zFar;
+		glm::vec3 cameraPos;//视图变换平移
+		glm::vec3 cameraFront;
+		glm::vec3 cameraUp;
+		
+		//std::unique_ptr<VertexArray> vao;//vertex buffer内存布局
+		//std::unique_ptr<VertexBuffer> vbo;
+		//std::unique_ptr<IndexBuffer> ibo;
+		//std::unique_ptr<Shader> shader;
+		//std::unique_ptr<Texture> texture;
+		//VertexBufferLayout layoutPosition;
+		Renderer m_renderer;
+
+	};
+
+}
