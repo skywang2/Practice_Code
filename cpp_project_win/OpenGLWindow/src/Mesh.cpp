8db8149f561c6d2d0a0866101da3eb0b66aa1446
skywang2 <727854256@qq.com>
2022-10-29 23:24:14 +0800

    [add][normal]1.增加Mesh类及其实现；2.增加使用Mesh进行顶点管理、渲染的3d模型、纹理等

--- cpp_project_win/OpenGLWindow/src/Mesh.cpp
+++ cpp_project_win/OpenGLWindow/src/Mesh.cpp
@@ -1,13 +1,71 @@
 #include "Mesh.h"
+#include <iostream>
+#include "GL/glew.h"
 
-Mesh::Mesh(std::vector<MeshVertex> vertices, std::vector<unsigned int> indices, std::vector<MeshVertex> textures)
+Mesh::Mesh(std::vector<MeshVertex> vertices, std::vector<unsigned int> indices, std::vector<MeshTexture> textures)
+    :VAO(0),
+    VBO(0),
+    EBO(0)
 {
+    this->vertices = vertices;
+    this->indices = indices;
+    this->textures = textures;
+
+    setupMesh();
 }
 
-void Mesh::Draw(Shader shader)
+void Mesh::Draw(Shader& shader)
 {
+    unsigned int diff = 1;
+    unsigned int spec = 1;
+    for (int i = 0; i < textures.size(); ++i)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+        std::string type = textures[i].type;
+        std::string num;
+        if ("texture_diffuse" == type)
+        {
+            num = std::to_string(diff++);
+        }
+        else if ("texture_specular" == type)
+        {
+            num = std::to_string(spec++);
+        }
+        //shader中有变量名为material的结构体
+        shader.SetUniform1i("material." + type + num, i);//给shader的纹理采样器赋值
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+    //glActiveTexture(GL_TEXTURE0);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 }
 
 void Mesh::setupMesh()
 {
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    //绑定gl的顶点数组与其buffer
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //给顶点数组对应buffer赋值，注意&vector和&vector[0]的地址是不同的
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(MeshVertex), &vertices[0], GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    //顶点
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(MeshVertex), (void*)offsetof(MeshVertex, position));
+    //法线
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(MeshVertex), (void*)offsetof(MeshVertex, normal));
+    //纹理坐标
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(MeshVertex), (void*)offsetof(MeshVertex, texcoords));
+
+    glBindVertexArray(0);
 }
--- cpp_project_win/OpenGLWindow/src/Mesh.h
+++ cpp_project_win/OpenGLWindow/src/Mesh.h
@@ -10,7 +10,7 @@ struct MeshVertex
 {
 	glm::vec3 position;
 	glm::vec3 normal;
-	glm::vec3 texcoords;
+	glm::vec2 texcoords;
 };
 
 struct MeshTexture
@@ -23,20 +23,20 @@ struct MeshTexture
 class Mesh
 {
 public:
-    /*  网格数据  */
+    //网格数据
     std::vector<MeshVertex> vertices;
     std::vector<unsigned int> indices;
     std::vector<MeshTexture> textures;
 
 public:
-    Mesh(std::vector<MeshVertex> vertices, std::vector<unsigned int> indices, std::vector<MeshVertex> textures);
-    void Draw(Shader shader);
+    Mesh(std::vector<MeshVertex> vertices, std::vector<unsigned int> indices, std::vector<MeshTexture> textures);
+    void Draw(Shader& shader);//使用指定着色器绘制网格
 
 private:
     unsigned int VAO, VBO, EBO;
 
 private:
-    void setupMesh();
+    void setupMesh();//初始化顶点缓冲
 
 };
 
