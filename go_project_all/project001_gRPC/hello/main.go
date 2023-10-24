9a7c099d5b8068f38702717d232009be7a2de7b4
skywang2 <727854256@qq.com>
2023-10-24 10:43:01 +0800

    [add][normal]1.给go grpc测试项目增加go.mod模式文件；2.写一个简单的grpc服务端程序

---
+++ go_project_all/project001_gRPC/hello/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld" // protobuf文件
+)
+
+var (
+	port = flag.Int("port", 8083, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) DoSomething(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello" + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	s.Serve(lis)
+}
+
